84e77a514502d6837405784d3bf1850560a7ac16
Vanessa Rivera <10712121+rivessa@users.noreply.github.com>
2024-02-11 17:00:14 -0800

    add Module5 code

---
+++ Module5/IntOp.java
@@ -0,0 +1,4 @@
+@FunctionalInterface // This annotation is optional
+public interface IntOp {
+    int operate(int x, int y);
+}
---
+++ Module5/Potion.java
@@ -0,0 +1,9 @@
+import java.util.function.Consumer;
+
+public class Potion {
+    public Consumer<PotionDrinker> effect;
+
+    public Potion(Consumer<PotionDrinker> effect) {
+        this.effect = effect;
+    }
+}
